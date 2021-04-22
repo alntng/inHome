857977b03725a5dd628273e5cf33f1fa3525287c
Alan <atang8013@gmail.com>
2021-04-22 12:41:15 -0400

    remove console logs and other cleanup

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,6 @@ const appRouter = (app, fs) => {
     res.send("welcome to the development api-server");
   });
 
-  // run our user route module here to complete the wire up
   customerRoutes(app, fs);
   orderRoutes(app, fs);
   itemRoutes(app, fs);
